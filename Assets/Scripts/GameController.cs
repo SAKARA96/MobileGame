005975d0bac182f078f913477e12eae3f04c185e
SAKARA96 <skarandi@usc.edu>
2020-02-18 14:54:29 -0800

    Added Spawning Code

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject rock;
+    public Vector3 spawnValue;
+    public int rockCount;
+    public float startWait;
+    public float spawnWait;
+    public float waveWait;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds (startWait);
+        while(true){
+            for(int i = 0; i < rockCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x,spawnValue.x),spawnValue.y,spawnValue.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(rock, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds (spawnWait);
+            }
+            yield return new WaitForSeconds (waveWait);
+        }
+    }
+
+}
